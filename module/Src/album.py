8864528731a8f4e2ed3ecccdcdaa07dce9f35efd
justin <justin_yih@hotmail.com>
2020-03-27 13:10:24 -0700

    format

--- module/Src/album.py
+++ module/Src/album.py
@@ -2,19 +2,20 @@ from .. import db
 
 
 class Album(db.Model):
-    __tablename__ = "album"
-    __id = db.Column(db.Integer, primary_key=True)
-    cover_img = db.Column(db.String(
+    __tablename__   = "album"
+    __id            = db.Column(db.Integer, primary_key=True)
+    cover_img       = db.Column(db.String(
         999), default="https://icons.iconarchive.com/icons/papirus-team/papirus-mimetypes/512/unknown-icon.png", unique=False, nullable=True)
-    name = db.Column(db.String(80), default="unknow",
+    name            = db.Column(db.String(80), default="unknow",
                      unique=True, nullable=True)
-    author = db.Column(db.String(80), default="unknow",
+    author          = db.Column(db.String(80), default="unknow",
                        unique=False, nullable=True)
-    genre = db.Column(db.String(80), default="unknow",
+    genre           = db.Column(db.String(80), default="unknow",
                       unique=False, nullable=True)
-    year = db.Column(db.Integer, default=9999, unique=False, nullable=True)
-    like = db.Column(db.Integer, default=0, unique=False, nullable=True)
-    Song_list = db.relationship("Song", backref="Album")
+    year            = db.Column(db.Integer, default=9999, unique=False, nullable=True)
+    like            = db.Column(db.Integer, default=0, unique=False, nullable=True)
+    Song_list       = db.relationship("Song", backref="Album")
 
-    # def __repr__(self):
-    #     return "sth"
+    def __repr__(self):
+        return '<Album {}>'.format(f"Album id: {self.id} Album name: {self.name}")
+
--- module/Src/user.py
+++ module/Src/user.py
@@ -4,12 +4,15 @@ import datetime
 
 
 class user(db.Model):
-    __id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(999), unique=False, nullable=True)
-    email = db.Column(db.String(999), unique=True, nullable=False)
-    password = db.Column(db.String(99999), unique=False, nullable=False)
-    date_of_birth = db.Column(
+    __id            = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(999), unique=False, nullable=True)
+    email           = db.Column(db.String(999), unique=True, nullable=False)
+    password        = db.Column(db.String(99999), unique=False, nullable=False)
+    date_of_birth   = db.Column(
         db.Date(), default="0000-00-00", unique=False, nullable=True)
-    favorite_song = db.Column(db.PickleType,
+    favorite_song   = db.Column(db.PickleType,
                               default=[], unique=False, nullable=True)
-    created_date = db.Column(DateTime, default=datetime.datetime.utcnow)
+    created_date    = db.Column(DateTime, default=datetime.datetime.utcnow)
+
+def __repr__(self):
+        return '<User {}>'.format(self.name) 
